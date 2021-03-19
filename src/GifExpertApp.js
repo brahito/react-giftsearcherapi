529dc4cada115a512c91907ae7a544e88f2cc047
brahito <brayantino24@hotmail.com>
2021-03-19 14:34:57 -0500

    correccion del build

---
+++ src/GifExpertApp.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['perritos']);
+    
+  
+    return (
+        <>         
+            <h2>Buscador de Gifs</h2>
+            <AddCategory setCategories = { setCategories }/> 
+            <hr/>
+
+
+            {
+                <ol> 
+                    { 
+                        categories.map( category => 
+                            <GifGrid
+                                key = { category }
+                                category={ category } 
+                            />
+                        )
+                    }
+                </ol>
+            }
+        </>
+    )
+}
